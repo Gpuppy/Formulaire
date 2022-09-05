fd5ef1b5abe59e997728571ec2ee1574a4dcf37f
Gael Adam <gaelswim@hotmail.com>
2022-09-05 21:15:55 +0200

    fifth version

--- main.js
+++ main.js
@@ -1,48 +1,80 @@
-        const form = document.getElementById("#form");
-        const firstName = document.getElementById('firstName');
-        const lastName = document.getElementById('lastName');
-        const email = document.getElementById('email');
-        const phone = document.getElementById('phone');
-        const address = document.getElementById('address');
-        const town = document.getElementById('town');
-        const postCode = document.getElementById('postCode');
-            
-        form.addEventListener('submit',e=>{
-            e.preventDefault();
         
-            form_verify();
-        })
-                
-                  
+        let firstName = document.getElementById("txtFirstName");
+        let lastName = document.getElementById("txtLastName");
+        let email = document.getElementById("txtEmail");
+        let phone = document.getElementById("txtPhone");
+        let address = document.getElementById("txtAddress");
+        let town = document.getElementById("txtTown");
+        let postCode = document.getElementById("txtPostCode");
+        let form = document.querySelector("form");
+
             
-    function form_verify() {
-        const nameValue = firstName.value.trim();
-
-
-
-        if(nameValue === ""){
-            let message = "firstName ne peut pas etre vide";
-            setError(firstName, message);
-        }else if(!nameValue.match(/^[a-zA-Z]/)){
-            let message = "firstName doit commencer par une lettre";
-            setError(firstName, message)
-        }else{
-            let letterNum = nameValue.length;
-            if(letterNum < 3) {
-                let message ="firstName doit avoir au moins 3 caratères";
-                setError(firstName, message)
-
-            }else{ setSucces(firstName);
-            }
+        function validateInput(){
+            console.log("validate input");
         }
-    }
+       
+        if(firstName.value.trim()===""){
+            onError(firstName, "User Name cannot be empty") ;
+        }else{
+            onSuccess(firstName);
+        }
 
-    function setError(elem, message){
-        const formControl = elem.parentElement;
-        const small = formControl.getElementById('small');
+        if(lastName.value.trim()===""){
+            onError(lastName, "Last Name cannot be empty") ;
+        }else{
+            onSuccess(lastName);
+        }
 
-        small.innerText = message
+        if(email.value.trim()===""){
+            onError(email, "Email is not valid") ;
+        }else{
+            onSuccess(email);
+        }
 
-        formControl.className = "form-control error";
-    }
-    
+        if(phone.value.trim()===""){
+            onError(phone, "Phone Number not valid") ;
+        }else{
+            onSuccess(phone);
+        }
+
+        if(address.value.trim()===""){
+            onError(address, "address cannot be empty") ;
+        }else{
+            onSuccess(address);
+        }
+
+        if(town.value.trim()===""){
+            onError(town, "town cannot be empty") ;
+        }else{
+            onSuccess(town);
+        }
+
+
+        document.querySelector("button")
+        .addEventListener("submit",()=>{
+            Event.preventDefault();
+            validateInput();
+        });
+
+        function onSuccess(input){
+            let parent=input.parentElement;
+            let messageEle=parent.querySelector("small");
+            messageEle.style.visibility="hidden";
+            parent.classList.remove("error");
+            parent.classList.add("success");
+        }
+
+        function onError(input){
+            let parent=input.parentElement;
+            let messageEle=parent.querySelector("small");
+            messageEle.style.visibility="visible";
+            messageEle.innerText="message";
+            parent.classList.add("error");
+            parent.classList.remove("success");
+        }
+
+        function isValidEmail(email){
+            return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+         }
+         
+         
